cbf6b202b74e4f72d8c72348af5d5af7f3b8a2e8
Robin Schoonover <robin@cornhooves.org>
2012-10-01 02:15:24 -0600

    Add/update ptBR, ruRU, frFR localizations.

---
+++ Locales/ptBR.lua
@@ -0,0 +1,22 @@
+
+--[[
+
+Translations are maintained at http://wow.curseforge.com/projects/battlepetcount/localization/
+
+Please update http://wow.curseforge.com/projects/battlepetcount/localization/ptBR/
+instead of modifying this file.
+
+--]]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("BattlePetCount", "ptBR")
+if not L then return end
+
+--
+
+L["OWNED"] = "J\195\161 Possui"
+L["PET_IS_UPGRADE"] = "Este mascote \195\169 uma melhoria de qualidade."
+L["UNOWNED"] = "N\195\163o Possui"
+L["YOU_DONT_OWN"] = "Voc\195\170 n\195\163o possui este mascote."
+L["YOU_OWN"] = "Voc\195\170 j\195\161 possui este mascote."
+L["YOU_OWN_COLON"] = "Voc\195\170 j\195\161 possui este mascote:"
+
