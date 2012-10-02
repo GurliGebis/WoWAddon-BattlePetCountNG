0b3a1bc82aacd51b26e63c20005c9f5b07b1c507
Robin Schoonover <robin@cornhooves.org>
2012-10-02 10:46:57 -0600

    Add zhTW localization.

---
+++ Locales/zhTW.lua
@@ -0,0 +1,21 @@
+
+--[[
+
+Translations are maintained at http://wow.curseforge.com/projects/battlepetcount/localization/
+
+Please update http://wow.curseforge.com/projects/battlepetcount/localization/zhTW/
+instead of modifying this file.
+
+--]]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("BattlePetCount", "zhTW")
+if not L then return end
+
+--
+
+L["OWNED"] = "\229\183\178\230\156\137"
+L["PET_IS_UPGRADE"] = "\230\175\148\230\151\165\232\170\140\229\133\167\231\154\132\233\171\152\229\147\129\232\179\170"
+L["UNOWNED"] = "\229\190\133\230\138\147"
+L["YOU_DONT_OWN"] = "\230\151\165\232\170\140\229\133\167\229\176\154\230\156\170\230\148\182\233\140\132"
+L["YOU_OWN"] = "\230\151\165\232\170\140\229\183\178\231\182\147\230\156\137\230\148\182\233\140\132"
+L["YOU_OWN_COLON"] = "\230\151\165\232\170\140\229\183\178\230\148\182\233\140\132: "
