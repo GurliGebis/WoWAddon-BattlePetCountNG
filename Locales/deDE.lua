a882058ae0df63f762666f2231ec66981cc88b6d
Robin Schoonover <robin@cornhooves.org>
2012-10-10 13:34:11 -0600

    Update deDE

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -13,9 +13,19 @@ if not L then return end
 
 --
 
+L["OPT_BATTLE_HINT_BOX"] = "Hinweistext im Kampf anzeigen"
+-- L["OPT_BATTLE_TIP"] = ""
+-- L["OPT_CAGE_TIP"] = ""
+-- L["OPT_CREATURE_TIP"] = ""
+L["OPT_HEADER_BATTLE"] = "Kampf"
+L["OPT_HEADER_ITEMS"] = "Gegenstand"
+L["OPT_HEADER_WORLD"] = "Welt"
+-- L["OPT_ITEM_TIP"] = ""
+L["OPT_ITEM_TIP_ALL"] = "Gegenstands Hinweis beinhaltet Nicht-Kampff\195\164hige Haustiere"
+-- L["OPT_MINIMAP_TIP"] = ""
 L["OWNED"] = "Im Besitz"
-L["PET_IS_UPGRADE"] = "Dieses Haustier ist besser."
 L["UNOWNED"] = "Nicht im Besitz"
+L["UPGRADE"] = "Verbesserung"
 L["YOU_DONT_OWN"] = "Du besitzt dieses Haustier nicht."
 L["YOU_OWN"] = "Du besitzt dieses Haustier."
-L["YOU_OWN_COLON"] = "Du besitzt dieses Haustier:" -- Needs review
+L["YOU_OWN_COLON"] = "Du besitzt dieses Haustier:"
