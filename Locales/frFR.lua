11b4b432a4c0cb9b6a3b76f6a38f3588141762d5
Robin Schoonover <robin@cornhooves.org>
2012-11-28 11:39:35 -0700

    Update all locales.

--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -13,21 +13,31 @@ if not L then return end
 
 --
 
-L["OPT_BATTLE_BORDER"] = "Modifier la couleur de bordure des mascottes de combat" -- Needs review
-L["OPT_BATTLE_BORDER_ICON"] = "Montrer l'ic\195\180ne d'info sur la bordure du portrait" -- Needs review
-L["OPT_BATTLE_HINT_BOX"] = "Montrer la zone d'infos en combat" -- Needs review
-L["OPT_BATTLE_TIP"] = "Modifier l'infobulle en combat" -- Needs review
-L["OPT_CAGE_TIP"] = "Modifier l'infobulle des mascottes mises en cage" -- Needs review
-L["OPT_CREATURE_TIP"] = "Modifier l'infobulle des cr\195\169atures" -- Needs review
-L["OPT_HEADER_BATTLE"] = "Combat" -- Needs review
-L["OPT_HEADER_ITEMS"] = "Objets" -- Needs review
-L["OPT_HEADER_WORLD"] = "Monde" -- Needs review
-L["OPT_ITEM_TIP"] = "Modifier l'infobulle des objets \195\160 apprendre" -- Needs review
-L["OPT_ITEM_TIP_ALL"] = "Inclure les Infobulle des mascottes non-combattantes" -- Needs review
-L["OPT_MINIMAP_TIP"] = "Modifier l'infobulle sur la mini-carte" -- Needs review
+-- L["EXPERIMENTAL_TAG"] = ""
+-- L["ITEM_PET_KNOWN_5_0"] = ""
+L["OPT_BATTLE_BORDER"] = "Modifier la couleur de bordure des mascottes de combat"
+L["OPT_BATTLE_BORDER_ICON"] = "Montrer l'ic\195\180ne d'info sur la bordure du portrait"
+L["OPT_BATTLE_HINT_BOX"] = "Montrer la zone d'infos en combat"
+L["OPT_BATTLE_TIP"] = "Modifier l'infobulle en combat"
+L["OPT_CAGE_TIP"] = "Modifier l'infobulle des mascottes mises en cage"
+L["OPT_CREATURE_TIP"] = "Modifier l'infobulle des cr\195\169atures"
+L["OPT_HEADER_BATTLE"] = "Combat"
+L["OPT_HEADER_ITEMS"] = "Objets"
+-- L["OPT_HEADER_OTHER"] = ""
+-- L["OPT_HEADER_STATS"] = ""
+L["OPT_HEADER_WORLD"] = "Monde"
+L["OPT_ITEM_TIP"] = "Modifier l'infobulle des objets \195\160 apprendre"
+L["OPT_ITEM_TIP_ALL"] = "Inclure les Infobulle des mascottes non-combattantes"
+L["OPT_MINIMAP_TIP"] = "Modifier l'infobulle sur la mini-carte"
+-- L["OPT_PET_COMPARE"] = ""
+-- L["OPT_PET_STATS"] = ""
+-- L["OPT_PREFER_NAMES_OVER_QUALITY"] = ""
+-- L["OPT_USE_OLDER_TEXT"] = ""
+-- L["OPT_USE_SUB_TIP"] = ""
 L["OWNED"] = "Poss\195\169d\195\169"
 L["UNOWNED"] = "Non poss\195\169d\195\169"
-L["UPGRADE"] = "Am\195\169lioration" -- Needs review
+L["UPGRADE"] = "Am\195\169lioration"
 L["YOU_DONT_OWN"] = "Pas encore poss\195\169d\195\169"
 L["YOU_OWN"] = "D\195\169j\195\160 poss\195\169d\195\169"
 L["YOU_OWN_COLON"] = "Vous poss\195\169dez cette mascotte : "
+
