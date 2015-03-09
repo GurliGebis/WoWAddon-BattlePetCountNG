dd0a3297d6d9f9d4f0eda805686816f543c0a80c
Robin Schoonover <robin@cornhooves.org>
2015-03-08 18:13:02 -0600

    Don't exclude achievement pets from missing scan, since they always have items.
    This didn't seem to always be the case, but /bpcmissing doesn't return
    anything new here.

--- CritterItems.lua
+++ CritterItems.lua
@@ -556,7 +556,7 @@ function SlashCmdList.BPCMISSINGSCAN(msg, editbox)
     for i,speciesID in LPJ:IterateSpeciesIDs() do
         if not speciesSet[speciesID] then
             local name, _, _, creatureID, sourceText, _, isWild, canBattle = C_PetJournal.GetPetInfoBySpeciesID(speciesID)
-            if not isWild and not strfind(sourceText, "Achievement:") and not EXCEPTIONS[speciesID] then
+            if not isWild and not EXCEPTIONS[speciesID] then
                 count = count + 1
                 print(format("%s species=%d creature=%d", name, speciesID, creatureID))
             end
