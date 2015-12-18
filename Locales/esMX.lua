e252f377b7e8613e30877c79e0982a7ede1a65e1
Robin Schoonover <robin@cornhooves.org>
2015-12-17 20:56:48 -0700

    Update esMX.

--- Locales/esMX.lua
+++ Locales/esMX.lua
@@ -13,6 +13,7 @@ if not L then return end
 
 --
 
+L["ALSO_OWN_COLON"] = "Tambi\195\169n posees:" -- Needs review
 L["EXPLAIN_BATTLE_HINT_DRAG"] = "Alt-clic para arrastrar la caja de sugerencias de duelo"
 L["OPT_BATTLE_BORDER"] = "Modificar el color del borde en duelos de mascotas"
 L["OPT_BATTLE_BORDER_ICON"] = "Mostrar \195\173cono de notificaci\195\179n en el borde del duelo de mascotas"
@@ -29,6 +30,7 @@ L["OPT_HEADER_WORLD"] = "Mundo"
 L["OPT_ITEM_TIP"] = "Modificar ventana emergente de art\195\173culos aprendibles"
 L["OPT_ITEM_TIP_ALL"] = "La ventana emergente de objetos incluye las mascotas que no pueden pelear"
 L["OPT_MINIMAP_TIP"] = "Modificar ventana emergente del Minimapa"
+L["OPT_PET_CARD"] = "Modificar tarjeta de mascota" -- Needs review
 L["OPT_PREFER_NAMES_OVER_QUALITY"] = "Prefiero mostrar el nombre de las mascotas"
 L["OPT_USE_BREEDID_ADDON"] = "Mostrar BreedID (Requiere Battle Pet BreedID)"
 L["OPT_USE_OLDER_TEXT"] = "Usar estilo de texto original en las capturadas."
