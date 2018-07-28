bc42c7ed70a78780c20240feb52c2fccccd873a9
Robin Schoonover <robin@cornhooves.org>
2018-07-28 13:36:21 -0600

    Use localization replacement

--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -13,29 +13,4 @@ if not L then return end
 
 --
 
--- L["EXPLAIN_BATTLE_HINT_DRAG"] = ""
-L["OPT_BATTLE_BORDER"] = "Alterar cor da borda no duelo de mascotes"
-L["OPT_BATTLE_BORDER_ICON"] = "Mostrar \195\173cone na borda do duelo de mascotes"
--- L["OPT_BATTLE_HINT_BOX"] = ""
--- L["OPT_BATTLE_HINT_RESET"] = ""
-L["OPT_BATTLE_TIP"] = "Alterar tooltips no duelo"
-L["OPT_CAGE_TIP"] = "Alterar tooltip do mascote capturado"
-L["OPT_CREATURE_TIP"] = "Alterar tooltips das criaturas"
-L["OPT_HEADER_BATTLE"] = "Duelo"
-L["OPT_HEADER_ITEMS"] = "Objetos"
-L["OPT_HEADER_OTHER"] = "Outro"
-L["OPT_HEADER_STATS"] = "Estat\195\173sticas"
-L["OPT_HEADER_WORLD"] = "Mundo"
--- L["OPT_ITEM_TIP"] = ""
--- L["OPT_ITEM_TIP_ALL"] = ""
-L["OPT_MINIMAP_TIP"] = "Alterar tooltips no minimapa"
--- L["OPT_PREFER_NAMES_OVER_QUALITY"] = ""
-L["OPT_USE_BREEDID_ADDON"] = "Mostrar BreedID (Requer Addon Battle Pet BreedID)"
--- L["OPT_USE_OLDER_TEXT"] = ""
-L["OPT_USE_SUB_TIP"] = "Usar um tooltip extra."
-L["OWNED"] = "Capturado"
-L["UNOWNED"] = "N\195\163o capturado"
-L["UPGRADE"] = "Atualizar"
-L["YOU_DONT_OWN"] = "N\195\163o capturado."
-L["YOU_OWN_COLON"] = "Capturado:"
-
+--@localization(locale="ptBR", format="lua_additive_table")@
